07ca91e73e653cc363eaf7bcba57bb057afb4cf6
Eric <Eric.p.83@gmail.com>
2020-12-27 11:41:50 +0100

    add some page

---
+++ gatsby-node.dev.js
@@ -0,0 +1,34 @@
+"use strict";
+
+function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); keys.push.apply(keys, symbols); } return keys; }
+
+function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(source, true).forEach(function (key) { _defineProperty(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(source).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
+/**
+ * Implement Gatsby's Node APIs in this file.
+ *
+ * See: https://www.gatsbyjs.com/docs/node-apis/
+ */
+// You can delete this file if you're not using it
+exports.onCreateWebpackConfig = function (_ref) {
+  var getConfig = _ref.getConfig,
+      stage = _ref.stage;
+  var actions = getConfig();
+
+  if (stage === "build-javascript") {
+    // Turn off source maps
+    actions.setWebpackConfig({
+      devtool: false
+    });
+  }
+
+  var config = getConfig();
+
+  if (stage.startsWith("develop") && config.resolve) {
+    config.resolve.alias = _objectSpread({}, config.resolve.alias, {
+      "react-dom": "@hot-loader/react-dom"
+    });
+  }
+};
--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,19 @@
  */
 
 // You can delete this file if you're not using it
+exports.onCreateWebpackConfig = ({ getConfig, stage }) => {
+  const actions = getConfig()
+  if (stage === "build-javascript") {
+    // Turn off source maps
+    actions.setWebpackConfig({
+      devtool: false,
+    })
+  }
+  const config = getConfig()
+  if (stage.startsWith("develop") && config.resolve) {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "react-dom": "@hot-loader/react-dom",
+    }
+  }
+}
